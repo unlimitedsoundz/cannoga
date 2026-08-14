bca1f9a1192e3c95825a28e7a7d4d8841c48a371
unlimitedsoundz <unlymitedsoundz@gmail.com>
2026-08-14 22:15:09 +0100

    Remove Login from Hero breadcrumbs header

--- src/app/portal/account/login/page.tsx
+++ src/app/portal/account/login/page.tsx
@@ -112,7 +112,7 @@ export default function PortalLoginPage() {
                     tinted
                     lightText={true}
                     image={{ src: '/images/international-students-hero.png', alt: 'Cannoga students' }}
-                    breadcrumbs={[{ label: 'Home', href: '/' }, { label: 'Portal', href: '/portal' }, { label: 'Login' }]}
+                    breadcrumbs={[{ label: 'Home', href: '/' }, { label: 'Portal', href: '/portal' }]}
                     className="min-h-[250px] lg:min-h-[350px]"
                 />
 
