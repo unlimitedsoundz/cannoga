aed388440fca05373f0c1220f8e2f7df7da60d6f
unlimitedsoundz <unlymitedsoundz@gmail.com>
2026-08-15 16:57:11 +0100

    feat(header): add Welcome from the President to About sub-nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -130,6 +130,7 @@ const navigation: NavItem[] = [
         href: "/about",
         children: [
             { name: "Our Story", href: "/about" },
+            { name: "Welcome from the President", href: "/about/welcome-from-the-president" },
             { name: "News & Events", href: "/news" },
             { name: "Research Hub", href: "/research" },
             { name: "Careers", href: "/careers" },
